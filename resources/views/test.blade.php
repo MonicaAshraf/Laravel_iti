6081969ffedceaa66884860f1e1560068967fe75
MonicaAshraf <monicaashraf534@gmail.com>
2022-04-16 22:15:08 +0200

    first commit

---
+++ resources/views/test.blade.php
@@ -0,0 +1,8 @@
+
+
+@foreach ($names as $name)
+    <p>This is name {{ $name }}</p> 
+    <!--   {{}} => mostach tamplet act as advanced echo    -->
+@endforeach
+
+
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+  //1St parameter:uri=>sub of url , 2nd parameter:action=>function that will be executed when go to the path /
+  //closure function or callback function
+    return view('welcome');//execute welcome.blade.php file
+    //view:global helper method=>make load for welcome.blade.php view , takes just name of view
+  // return 'monica'; 
 });
+
+
+Route::get('/test', function () {
+    $allNames=['monica', 'ashraf' , 'refaat'];
+    return view('test',[
+        'names' => $allNames,
+    ]);//1st parameter:"view",2nd parameter "data" :array[] ---> ('key' of foreach in view => value variable name of array)
+ });
