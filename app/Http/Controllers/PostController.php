ccb2406369574326a98d68f1adc4b5024db96b22
MonicaAshraf <monicaashraf534@gmail.com>
2022-04-18 02:14:08 +0200

    Add retriving single  posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,17 +8,25 @@ class PostController extends Controller
 {
     public function index()
     {
-    //STATIC_DATA
+    //STATIC_DATA V1
       //  $posts=[
       //    ['id' =>1, 'title' => 'Laravel', 'post_creator' =>'monica', 'created_at' =>'2022-04-16 2:10:00'],
       //    ['id' =>2, 'title' => 'Php', 'post_creator' =>'asmaa', 'created_at' =>'2022-04-16 3:10:00'],
       //    ['id' =>3, 'title' => 'Java', 'post_creator' =>'hadeer', 'created_at' =>'2022-04-16 4:10:00']
       //  ];
 
-    //DATABASE
+    //DATABASE V2:
      $posts= Post::all();//select * from posts; --> return object
       //dd($posts);//global helper method for debugging
       
+
+    //  $filteredPosts=Post::where('title','java');//select * from posts where title = 'java';
+    //  dd($filteredPosts);//return object of Eloquent\Builder --> because it don't know end of sql query
+    
+    $filteredPosts=Post::where('title','java')->get();//get() --> represent end of query
+    //dd($filteredPosts);//return object of Eloquent\Collection
+
+
        return view('posts.index',[
          'posts' => $posts,
          ]);//1st parameter:"view",2nd parameter "data" :array[] ---> ('key' of foreach in view => value variable name of array)
@@ -33,13 +41,24 @@ class PostController extends Controller
     }
 
     public function show($postId){
-      $posts=[
-        ['id' =>1, 'title' => 'Laravel', 'post_creator' =>'monica', 'created_at' =>'2022-04-16 2:10:00'],
-        ['id' =>2, 'title' => 'Php', 'post_creator' =>'asmaa', 'created_at' =>'2022-04-16 3:10:00'],
-        ['id' =>3, 'title' => 'Java', 'post_creator' =>'hadeer', 'created_at' =>'2022-04-16 4:10:00']
-      ];
+    //STATIC_DATA V1:
+      // $posts=[
+      //   ['id' =>1, 'title' => 'Laravel', 'post_creator' =>'monica', 'created_at' =>'2022-04-16 2:10:00'],
+      //   ['id' =>2, 'title' => 'Php', 'post_creator' =>'asmaa', 'created_at' =>'2022-04-16 3:10:00'],
+      //   ['id' =>3, 'title' => 'Java', 'post_creator' =>'hadeer', 'created_at' =>'2022-04-16 4:10:00']
+      // ];
+    
+    //DATABASE V2 :
+    //  $post = Post::where('id',$postId)->get();//select * from posts where id = 'postId';
+    //  dd($post);//return object of Eloquent\Collection
 
-    return view('posts.show',['post'=> $posts[$postId-1]]);
+    // $post = Post::where('id',$postId)->first(); 
+    // dd($post);//return object of Eloquent\Post
+
+    $post = Post::find($postId);//shorthand way
+    dd($post);
+
+   // return view('posts.show',['post'=> $posts[$postId-1]]);
     }
 
 
