5273321f4624448c4a89f0f207dc95042cab1c4a
MonicaAshraf <monicaashraf534@gmail.com>
2022-04-24 23:00:14 +0200

    Add PostResource

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -5,16 +5,27 @@ use App\Models\Post;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Requests\StorePostRequest;
+use App\Http\Resources\PostResource;
 class PostController extends Controller
 {
     public function index()
     {
-        return Post::all();
+        $posts=Post::all();
+        return PostResource::collection($posts);
     }
 
     public function show($postId)
     {
-        return Post::find($postId);
+        $post=Post::find($postId);
+
+        return new PostResource($post);
+        // return [
+        //     'id' => $post->id,
+        //     'title' => $post->title,
+        //     'description' => $post->description,
+        //     'user_id' => $post->user_id,
+            
+        // ];
     }
 
     public function store(StorePostRequest $request)
@@ -28,6 +39,14 @@ class PostController extends Controller
           //  'slug' =>Str::slug($request->input('title')),
             
           ]);
-          return $post;
+        //   return  [
+        //     'id' => $post->id,
+        //     'title' => $post->title,
+        //     'description' => $post->description,
+        //     'user_id' => $post->user_id,
+            
+        // ];
+
+        return new PostResource($post);
     }
 }
